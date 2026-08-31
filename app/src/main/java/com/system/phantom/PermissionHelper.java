20ae40c6f9ce81e0d911b12cb8e06c87da298ec6
endrowasito <80339706+endrowasito@users.noreply.github.com>
2026-08-31 17:23:12 +0700

    Update PermissionHelper.java

--- app/src/main/java/com/system/phantom/PermissionHelper.java
+++ app/src/main/java/com/system/phantom/PermissionHelper.java
@@ -1,16 +1,6 @@
 package com.system.phantom;
 import android.app.Activity;
-import android.content.Intent;
-import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
 public class PermissionHelper extends Activity {
-    @Override protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        MediaProjectionManager mgr = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
-        startActivityForResult(mgr.createScreenCaptureIntent(), 999);
-    }
-    @Override protected void onActivityResult(int req, int res, Intent data) {
-        super.onActivityResult(req, res, data);
-        finish();
-    }
+    @Override protected void onCreate(Bundle b) { super.onCreate(b); finish(); }
 }
